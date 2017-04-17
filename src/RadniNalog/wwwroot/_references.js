06d743f60b80e8846af1fb0fdf87a98a510efb0c
gzr <bleksabat666@gmail.com>
2017-04-17 20:09:47 +0200

    Added bootseatch

--- src/RadniNalog/wwwroot/_references.js
+++ src/RadniNalog/wwwroot/_references.js
@@ -7,12 +7,13 @@
 /// <reference path="controllers/chartcontroller2.js" />
 /// <reference path="controllers/createautomobilcontroller.js" />
 /// <reference path="controllers/createcontroller.js" />
-/// <reference path="controllers/createmjestoradacontroller - copy.js" />
+/// <reference path="controllers/CreateMjestoRadaController.js" />
 /// <reference path="controllers/createvrstaradacontroller.js" />
 /// <reference path="controllers/HomeController.js" />
 /// <reference path="controllers/mjestoradacontroller.js" />
 /// <reference path="controllers/radnicicontroller.js" />
 /// <reference path="controllers/vrstaradacontroller.js" />
+/// <reference path="js/angular-datepicker.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/angular/angular.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
@@ -20,6 +21,6 @@
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
 /// <reference path="services/automobilservice.js" />
-/// <reference path="services/mjestoradaservice - copy.js" />
+/// <reference path="services/mjestoRadaService.js" />
 /// <reference path="services/vrstaradaservice.js" />
 /// <reference path="services/zaposlenikService.js" />
